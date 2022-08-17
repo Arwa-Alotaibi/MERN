024902eb2045e9a45bf228871ecc989dca634de9
Arwa-Alotaibi <arwa.alosaimi@outlook.sa>
2022-08-17 23:04:10 +0300

    firstjsx

---
+++ Intro_to_React/firstjsx/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+    <li>Learn React </li>
+    <li>Climb Mt. Everest </li>
+    <li>Run a marathons </li>
+    <li>Feed the dojos </li>
+
+  </ul>
+  </>
+    
+  );
+}
+export default App;
+
