5330d9b5edd56191073a0d2d34d2640be4f94980
Arwa-Alotaibi <arwa.alosaimi@outlook.sa>
2022-08-24 00:01:44 +0300

    Big Inversion

---
+++ Functional_Components/big_inversion/src/components/PersonCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const PersonCard = props => {
+
+    return (
+        <>
+
+            <h1>{props.firstName} , {props.lastName}</h1>
+            <p>Age: {props.age}</p>
+            <p>Hair Color  : {props.hairColor}</p>
+
+        </>
+
+
+
+
+
+    );
+
+}
+
+export default PersonCard;
